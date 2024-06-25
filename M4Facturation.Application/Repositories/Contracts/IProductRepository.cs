a54d2ea402dc419e6508e6d301e0872eb14e7699
FABIOMERCADO <fabiomercadon@gmail.com>
2024-06-25 19:42:20 -0300

    Product repository is created.

---
+++ M4Facturation.Application/Repositories/Contracts/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace M4Facturation.Application.Repositories.Contracts
+{
+    public interface IProductRepository:IRepository<Products>
+    {
+        
+    }
+}
---
+++ M4Facturation.Application/Repositories/Implementations/ProductRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace M4Facturation.Application.Repositories.Implementations
+{
+    public class ProductRepository(PointSaleContext context, ICacheService cacheService, IMapper mapper) : Repository<Products>(context, cacheService, mapper), IProductRepository
+    {
+        
+    }
+}
